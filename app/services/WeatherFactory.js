f9a9e785fd0493cd865fe1897c723fcd1fb92524
JorisDek <dragster66@gmail.com>
2016-01-25 01:00:31 +0100

    APPID

--- app/services/WeatherFactory.js
+++ app/services/WeatherFactory.js
@@ -4,7 +4,7 @@ angular.module('weatherApp')
                 getWeather: function(){
                     var promise = $http({
                         method: 'GET',
-                        url: 'api.openweathermap.org/data/2.5/weather?q=London'
+                        url: 'api.openweathermap.org/data/2.5/weather?q=London&APPID=8fac16c780bdac0a55b68a2f937b949a'
                     });
                     promise.succes(function(data, status, headers, conf){
                         $scope.Weather = data;
