f412e010c6556d10c0d5ad362b1d4e1c7b4c6d45
JorisDek <dragster66@gmail.com>
2016-01-29 01:51:42 +0100

    endpoint verander naar forecast en icons toegevoegd, moeten nog in de view

--- app/controllers/weatherController.js
+++ app/controllers/weatherController.js
@@ -1,9 +1,9 @@
 
 angular.module('weatherApp')
-    .controller('WeatherCtrl', ['$scope', '$http', function($scope, $http){
+    .controller('WeatherCtrl', ['$scope', '$http',  function($scope, $http){          
         $http({
             method: 'GET',
-            url: 'http://api.openweathermap.org/data/2.5/weather?q=DenHaag&units=metric&APPID=8fac16c780bdac0a55b68a2f937b949a'
+            url: 'http://api.openweathermap.org/data/2.5/forecast?q=Voorburg&units=metric&lang=nl&APPID=8fac16c780bdac0a55b68a2f937b949a'
         }).then(function (response) {
             $scope.status = response.status;
             $scope.data = response.data;
