00f79478877a8cc2a2aedd8d389c6d504454a558
JorisDek <dragster66@gmail.com>
2016-01-30 02:24:15 +0100

    Factory gemaakt

--- app/controllers/weatherController.js
+++ app/controllers/weatherController.js
@@ -1,6 +1,6 @@
 
 angular.module('weatherApp')
-    .controller('WeatherCtrl', ['$scope', '$http',  function($scope, $http){          
+    .controller('WeatherCtrl', ['$scope', '$http', function($scope, $http){             
         $http({
             method: 'GET',
             url: 'http://api.openweathermap.org/data/2.5/forecast?q=Voorburg&units=metric&lang=nl&APPID=8fac16c780bdac0a55b68a2f937b949a'
--- app/services/WeatherFactory.js
+++ app/services/WeatherFactory.js
@@ -1,12 +1,26 @@
-angular.factory('WeatherFactory'['$http', '$scope', function($http, $scope){
-    $http({
-        method: 'GET',
-        url: 'http://api.openweathermap.org/data/2.5/weather?q=London&APPID=8fac16c780bdac0a55b68a2f937b949a'
-    }).then(function (response) {
-        $scope.status = response.status;
-        $scope.data = response.data;
-    }, function (response) {
-        return response;
-    });
+angular.factory('WeatherFactory', ['$http', function($http){
+    var WeatherFactory = function(){
+        this.initialize = function(){
+            var url = 'http://api.openweathermap.org/data/2.5/weather?q=London&APPID=8fac16c780bdac0a55b68a2f937b949a';
+            var forecastData = http.get(url);
+            var self = this;
+            
+            forecastData.then(function(response){
+                angular.extend(self, response.data);  
+            });
+        };
+        this.initialize();
+    };
+    
+    return (WeatherFactory);    
+//    $http({
+//        method: 'GET',
+//        url: 'http://api.openweathermap.org/data/2.5/weather?q=London&APPID=8fac16c780bdac0a55b68a2f937b949a'
+//    }).then(function (response) {
+//        $scope.status = response.status;
+//        $scope.data = response.data;
+//    }, function (response) {
+//        return response;
+//    });
 }]);
 
